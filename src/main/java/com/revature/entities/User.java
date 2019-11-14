eadba0ea94f30037b19ff90246bac118ff99032f
James Meadows <jamesmeadows87@gmail.com>
2019-11-14 18:47:08 -0500

    renamed user as customers for to fix sql error

--- src/main/java/com/revature/entities/User.java
+++ src/main/java/com/revature/entities/User.java
@@ -2,6 +2,7 @@ package com.revature.entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,12 +10,17 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 @Entity
+@Table(name="customers")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
 	private String email;
 	private String password;
 	@ManyToOne
