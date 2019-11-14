b925a9939895f7077993f7524f756268e220fce2
James Meadows <jamesmeadows87@gmail.com>
2019-11-14 17:52:14 -0500

    table creation working on local database

--- src/main/java/com/revature/entities/Service.java
+++ src/main/java/com/revature/entities/Service.java
@@ -6,11 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name="services")
@@ -33,6 +33,106 @@ public class Service {
 	private Vehicle vehicle;
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((technicianNote == null) ? 0 : technicianNote.hashCode());
+		result = prime * result + ((time == null) ? 0 : time.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((userNote == null) ? 0 : userNote.hashCode());
+		result = prime * result + ((vehicle == null) ? 0 : vehicle.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Service other = (Service) obj;
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (technicianNote == null) {
+			if (other.technicianNote != null)
+				return false;
+		} else if (!technicianNote.equals(other.technicianNote))
+			return false;
+		if (time == null) {
+			if (other.time != null)
+				return false;
+		} else if (!time.equals(other.time))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (userNote == null) {
+			if (other.userNote != null)
+				return false;
+		} else if (!userNote.equals(other.userNote))
+			return false;
+		if (vehicle == null) {
+			if (other.vehicle != null)
+				return false;
+		} else if (!vehicle.equals(other.vehicle))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Service [id=" + id + ", name=" + name + ", type=" + type + ", cost=" + cost + ", time=" + time
+				+ ", technicianNote=" + technicianNote + ", userNote=" + userNote + ", vehicle=" + vehicle + ", user="
+				+ user + "]";
+	}
+
+
+	public Service() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Service(int id, String name, ServiceType type, double cost, LocalDateTime time, String technicianNote,
+			String userNote, Vehicle vehicle, User user) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.cost = cost;
+		this.time = time;
+		this.technicianNote = technicianNote;
+		this.userNote = userNote;
+		this.vehicle = vehicle;
+		this.user = user;
+	}
+
+
 	//Is this meant to represent the technician
 	@ManyToOne
 	@JoinColumn(name="user_id")
