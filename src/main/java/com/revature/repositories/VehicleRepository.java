7dc40d9c0f8e738f64a51fe350b6c3e9b145d70e
Corey S <coreyschotte@gmail.com>
2019-11-15 15:07:33 -0500

    Created vehicle controller, service, and repository structure

---
+++ src/main/java/com/revature/repositories/VehicleRepository.java
@@ -0,0 +1,27 @@
+package com.revature.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import com.revature.entities.Vehicle;
+
+public class VehicleRepository {
+
+	public Vehicle create(@Valid Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Vehicle> getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<List<Vehicle>> getByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/revature/services/VehicleService.java
@@ -0,0 +1,45 @@
+package com.revature.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.revature.entities.Vehicle;
+import com.revature.repositories.VehicleRepository;
+
+public class VehicleService {
+	
+	VehicleRepository vehicleRepository;
+
+	@Autowired
+	public VehicleService(VehicleRepository vehicleRepository) {
+		super();
+		this.vehicleRepository = vehicleRepository;
+	}
+
+	public Vehicle getVehicle(int id) {
+		Optional<Vehicle> optionalVehicle = vehicleRepository.getById(id);
+		
+		return optionalVehicle.orElseThrow(
+				() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+	}
+
+	@Transactional
+	public Vehicle createVehicle(@Valid Vehicle vehicle) {
+		return vehicleRepository.create(vehicle);
+	}
+
+	public List<Vehicle> getUserVehicles(int userId) {
+		Optional<List<Vehicle>> optionalUserVehicles = vehicleRepository.getByUserId(userId);
+		
+		return optionalUserVehicles.orElseThrow(
+				() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+	}
+
+}
