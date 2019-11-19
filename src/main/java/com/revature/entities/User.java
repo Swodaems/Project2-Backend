4faeaa3be0ebca561990dda228bee961f6e8eb24
Corey S <coreyschotte@gmail.com>
2019-11-19 14:19:43 -0500

    added exception handler to tests

--- src/main/java/com/revature/entities/User.java
+++ src/main/java/com/revature/entities/User.java
@@ -14,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 @Table(name="customers")
 public class User {
