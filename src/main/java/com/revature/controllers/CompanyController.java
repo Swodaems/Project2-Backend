db182b911dcd2da29e68df0cc6e0c0be76525bbc
Corey S <coreyschotte@gmail.com>
2019-11-20 20:26:20 -0500

    added jwt authentication to controllers

--- src/main/java/com/revature/controllers/CompanyController.java
+++ src/main/java/com/revature/controllers/CompanyController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.revature.entities.Company;
+import com.revature.models.Creds;
 import com.revature.services.CompanyService;
+import com.revature.util.AuthUtil;
 
 @RestController
 @RequestMapping("company")
@@ -28,22 +32,30 @@ public class CompanyController {
 	}	
 
 	@GetMapping("/{id}")
-	public Company getCompany(@PathVariable int id) {
+	public Company getCompany(@PathVariable int id, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return companyService.getCompanyById(id);
 	}
 
 	@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Company createCompany(@RequestBody @Valid Company company) {
+	public Company createCompany(@RequestBody @Valid Company company, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return companyService.createCompany(company);
 	}
 	@PutMapping
-	public Company updateCompany(@RequestBody @Valid Company company) {
+	public Company updateCompany(@RequestBody @Valid Company company, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return companyService.updateCompany(company);
 	}
 	
 	@DeleteMapping
-	public Company deleteCompany(@RequestBody @Valid Company company) {
+	public Company deleteCompany(@RequestBody @Valid Company company, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return companyService.deleteCompany(company);
 	}
 }
--- src/main/java/com/revature/controllers/InsuranceController.java
+++ src/main/java/com/revature/controllers/InsuranceController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.revature.entities.Insurance;
+import com.revature.models.Creds;
 import com.revature.services.InsuranceService;
+import com.revature.util.AuthUtil;
 
 @RestController
 @RequestMapping("/insurance/")
@@ -31,22 +35,30 @@ public class InsuranceController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Insurance createInsurance(@RequestBody @Valid Insurance insurance) {
+	public Insurance createInsurance(@RequestBody @Valid Insurance insurance, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return insuranceService.createInsurance(insurance);
 	}
 	
 	@GetMapping("/{id}")
-	public Insurance getInsurance(@PathVariable int id) {
+	public Insurance getInsurance(@PathVariable int id, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return insuranceService.getInsurance(id);
 	}
 	
 	@PutMapping
-	public Insurance updateInsurance(@RequestBody @Valid Insurance insurance) {
+	public Insurance updateInsurance(@RequestBody @Valid Insurance insurance, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return insuranceService.updateInsurance(insurance);
 	}
 	
 	@DeleteMapping
-	public Insurance deleteInsurance(@RequestBody @Valid Insurance insurance) {
+	public Insurance deleteInsurance(@RequestBody @Valid Insurance insurance, @RequestHeader("Authorization") String token) {
+		Creds cred = AuthUtil.parseJWT(token);
+		if(cred == null) throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
 		return insuranceService.deleteInsurance(insurance);
 	}
 	
