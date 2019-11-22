a0959a973a41643d5fea61c7430743d36c219fc1
Corey S <coreyschotte@gmail.com>
2019-11-22 14:31:48 -0500

    fixed updateVehicle to re-apply old values upon update

--- src/main/java/com/revature/models/VehicleData.java
+++ src/main/java/com/revature/models/VehicleData.java
@@ -117,7 +117,7 @@ public class VehicleData {
 		// TODO Auto-generated constructor stub
 	}
 	public VehicleData(Vehicle vehicle) {
-		this.setUserId(vehicle.getUser().getId());
+		if(vehicle.getUser() != null) this.setUserId(vehicle.getUser().getId());
 		this.setName(vehicle.getName());
 		if(vehicle.getInsurance() != null) this.setInsuranceId(vehicle.getInsurance().getId());
 		List<Integer> serviceReportIds=new ArrayList<>();
